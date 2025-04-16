d2d23137578de9d0ebb6600664db761c8adf8cc6
Ibrat Ibragimov <71783256+iibratik@users.noreply.github.com>
2025-04-16 17:57:30 +0500

    fixing bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/test-utils', '@pinia/nuxt',],
   css: [
-    './assets/style/main.scss'  // Пути к твоим глобальным стилям
+    './assets/style/main.scss'
   ],
 
 })
